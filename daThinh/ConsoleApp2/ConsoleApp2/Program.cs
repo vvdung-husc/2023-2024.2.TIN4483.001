1e2f085b92dd6417707eceb1f93d4d1b8869b7af
AnThinh1 <144500323+AnThinh1@users.noreply.github.com>
2024-02-19 15:49:50 +0700

    athinhhhhh

--- daThinh/ConsoleApp2/ConsoleApp2/Program.cs
+++ daThinh/ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world !");
+            Console.WriteLine("Hello world !DU AN THINH");
         }
     }
 }
