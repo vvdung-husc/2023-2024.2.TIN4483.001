e9a02e75b91355d32ae19d57128f0c2ec1ce1a77
quocbao <bao29072000@gmail.com>
2024-02-28 09:44:09 +0700

    bao

--- bao/WindowsAppTwo/Form1.Designer.cs
+++ bao/WindowsAppTwo/Form1.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtNumA = new System.Windows.Forms.TextBox();
             this.txtNumB = new System.Windows.Forms.TextBox();
+            this.txtNumA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radUSCLN = new System.Windows.Forms.RadioButton();
-            this.radBSCNN = new System.Windows.Forms.RadioButton();
+            this.chkBSCNN = new System.Windows.Forms.RadioButton();
+            this.chkUSCLN = new System.Windows.Forms.RadioButton();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.btntim = new System.Windows.Forms.Button();
-            this.btnxoa = new System.Windows.Forms.Button();
-            this.bntthoat = new System.Windows.Forms.Button();
             this.txtketqua = new System.Windows.Forms.TextBox();
+            this.bntthoat = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btntim = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -59,14 +59,19 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Nhâp:";
             // 
-            // label1
+            // txtNumB
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 40);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "A:";
+            this.txtNumB.Location = new System.Drawing.Point(39, 75);
+            this.txtNumB.Name = "txtNumB";
+            this.txtNumB.Size = new System.Drawing.Size(253, 27);
+            this.txtNumB.TabIndex = 1;
+            // 
+            // txtNumA
+            // 
+            this.txtNumA.Location = new System.Drawing.Point(39, 40);
+            this.txtNumA.Name = "txtNumA";
+            this.txtNumA.Size = new System.Drawing.Size(253, 27);
+            this.txtNumA.TabIndex = 2;
             // 
             // label2
             // 
@@ -77,52 +82,50 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "B:";
             // 
-            // txtNumA
+            // label1
             // 
-            this.txtNumA.Location = new System.Drawing.Point(39, 40);
-            this.txtNumA.Name = "txtNumA";
-            this.txtNumA.Size = new System.Drawing.Size(253, 27);
-            this.txtNumA.TabIndex = 2;
-            // 
-            // txtNumB
-            // 
-            this.txtNumB.Location = new System.Drawing.Point(39, 75);
-            this.txtNumB.Name = "txtNumB";
-            this.txtNumB.Size = new System.Drawing.Size(253, 27);
-            this.txtNumB.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A:";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.radBSCNN);
-            this.groupBox2.Controls.Add(this.radUSCLN);
+            this.groupBox2.Controls.Add(this.chkBSCNN);
+            this.groupBox2.Controls.Add(this.chkUSCLN);
             this.groupBox2.Location = new System.Drawing.Point(414, 46);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 124);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Chọn";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
-            // radUSCLN
+            // chkBSCNN
             // 
-            this.radUSCLN.AutoSize = true;
-            this.radUSCLN.Location = new System.Drawing.Point(29, 40);
-            this.radUSCLN.Name = "radUSCLN";
-            this.radUSCLN.Size = new System.Drawing.Size(92, 24);
-            this.radUSCLN.TabIndex = 0;
-            this.radUSCLN.TabStop = true;
-            this.radUSCLN.Text = "USCLN";
-            this.radUSCLN.UseVisualStyleBackColor = true;
+            this.chkBSCNN.AutoSize = true;
+            this.chkBSCNN.Location = new System.Drawing.Point(27, 70);
+            this.chkBSCNN.Name = "chkBSCNN";
+            this.chkBSCNN.Size = new System.Drawing.Size(94, 24);
+            this.chkBSCNN.TabIndex = 1;
+            this.chkBSCNN.TabStop = true;
+            this.chkBSCNN.Text = "BSCNN";
+            this.chkBSCNN.UseVisualStyleBackColor = true;
+            this.chkBSCNN.CheckedChanged += new System.EventHandler(this.chkBSCNN_CheckedChanged);
             // 
-            // radBSCNN
+            // chkUSCLN
             // 
-            this.radBSCNN.AutoSize = true;
-            this.radBSCNN.Location = new System.Drawing.Point(27, 70);
-            this.radBSCNN.Name = "radBSCNN";
-            this.radBSCNN.Size = new System.Drawing.Size(94, 24);
-            this.radBSCNN.TabIndex = 1;
-            this.radBSCNN.TabStop = true;
-            this.radBSCNN.Text = "BSCNN";
-            this.radBSCNN.UseVisualStyleBackColor = true;
+            this.chkUSCLN.AutoSize = true;
+            this.chkUSCLN.Location = new System.Drawing.Point(29, 40);
+            this.chkUSCLN.Name = "chkUSCLN";
+            this.chkUSCLN.Size = new System.Drawing.Size(92, 24);
+            this.chkUSCLN.TabIndex = 0;
+            this.chkUSCLN.TabStop = true;
+            this.chkUSCLN.Text = "USCLN";
+            this.chkUSCLN.UseVisualStyleBackColor = true;
+            this.chkUSCLN.CheckedChanged += new System.EventHandler(this.chkUSCLN_CheckedChanged);
             // 
             // groupBox3
             // 
@@ -137,23 +140,12 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Kết quả:";
             // 
-            // btntim
+            // txtketqua
             // 
-            this.btntim.Location = new System.Drawing.Point(10, 86);
-            this.btntim.Name = "btntim";
-            this.btntim.Size = new System.Drawing.Size(115, 35);
-            this.btntim.TabIndex = 0;
-            this.btntim.Text = "Tìm";
-            this.btntim.UseVisualStyleBackColor = true;
-            // 
-            // btnxoa
-            // 
-            this.btnxoa.Location = new System.Drawing.Point(149, 86);
-            this.btnxoa.Name = "btnxoa";
-            this.btnxoa.Size = new System.Drawing.Size(114, 35);
-            this.btnxoa.TabIndex = 1;
-            this.btnxoa.Text = "Xóa";
-            this.btnxoa.UseVisualStyleBackColor = true;
+            this.txtketqua.Location = new System.Drawing.Point(39, 39);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.Size = new System.Drawing.Size(253, 27);
+            this.txtketqua.TabIndex = 3;
             // 
             // bntthoat
             // 
@@ -163,13 +155,27 @@
             this.bntthoat.TabIndex = 2;
             this.bntthoat.Text = "Thoát";
             this.bntthoat.UseVisualStyleBackColor = true;
+            this.bntthoat.Click += new System.EventHandler(this.bntthoat_Click);
             // 
-            // txtketqua
+            // btnxoa
             // 
-            this.txtketqua.Location = new System.Drawing.Point(39, 39);
-            this.txtketqua.Name = "txtketqua";
-            this.txtketqua.Size = new System.Drawing.Size(253, 27);
-            this.txtketqua.TabIndex = 3;
+            this.btnxoa.Location = new System.Drawing.Point(149, 86);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(114, 35);
+            this.btnxoa.TabIndex = 1;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btntim
+            // 
+            this.btntim.Location = new System.Drawing.Point(10, 86);
+            this.btntim.Name = "btntim";
+            this.btntim.Size = new System.Drawing.Size(115, 35);
+            this.btntim.TabIndex = 0;
+            this.btntim.Text = "Tìm";
+            this.btntim.UseVisualStyleBackColor = true;
+            this.btntim.Click += new System.EventHandler(this.btntim_Click);
             // 
             // Form1
             // 
@@ -180,7 +186,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Tìm USCLN và BSCNN của A và B";
             this.groupBox1.ResumeLayout(false);
@@ -201,8 +207,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton radBSCNN;
-        private System.Windows.Forms.RadioButton radUSCLN;
+        private System.Windows.Forms.RadioButton chkBSCNN;
+        private System.Windows.Forms.RadioButton chkUSCLN;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TextBox txtketqua;
         private System.Windows.Forms.Button bntthoat;
--- bao/WindowsAppTwo/Form1.cs
+++ bao/WindowsAppTwo/Form1.cs
@@ -16,5 +16,48 @@ namespace WindowsAppTwo
         {
             InitializeComponent();
         }
+
+        private void bntthoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void btntim_Click(object sender, EventArgs e)
+        {
+            if (chkUSCLN.Checked)
+            {
+                MessageBox.Show("Đang chọn USCLN => Tính kết quả", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (chkBSCNN.Checked)
+            {
+                MessageBox.Show("Đang chọn BSCNN => Tính kết quả", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn tìm USCLN hay BSCNN", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+            btntim.Text = "Tìm - USCLN";
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            txtNumA.Text = "";
+            txtNumB.Text = "";
+        }
+
+        private void chkUSCLN_CheckedChanged(object sender, EventArgs e)
+        {
+           btntim.Text = "Tìm - USCLN";
+        }
+
+        private void chkBSCNN_CheckedChanged(object sender, EventArgs e)
+        {
+            btntim.Text = "Tìm - BSCNN";
+        }
     }
 }
