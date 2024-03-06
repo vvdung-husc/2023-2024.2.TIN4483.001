804ff4ea1c627f52b91fef25220ef2ef4acd172d
thaotien0202 <20T1020583@husc.edu.vn>
2024-03-06 09:57:24 +0700

    commit

--- tntan/WindowAppThree/WindowAppThree/Form1.Designer.cs
+++ tntan/WindowAppThree/WindowAppThree/Form1.Designer.cs
@@ -38,10 +38,9 @@
             // 
             this.dgvMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvMember.Location = new System.Drawing.Point(26, 75);
-            this.dgvMember.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvMember.Margin = new System.Windows.Forms.Padding(2);
             this.dgvMember.Name = "dgvMember";
             this.dgvMember.RowHeadersWidth = 51;
-            this.dgvMember.RowTemplate.Height = 24;
             this.dgvMember.Size = new System.Drawing.Size(781, 344);
             this.dgvMember.TabIndex = 0;
             this.dgvMember.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMember_CellContentClick);
@@ -49,38 +48,35 @@
             // radList
             // 
             this.radList.AutoSize = true;
-            this.radList.Location = new System.Drawing.Point(26, 10);
-            this.radList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radList.Location = new System.Drawing.Point(31, 12);
             this.radList.Name = "radList";
             this.radList.Size = new System.Drawing.Size(144, 17);
             this.radList.TabIndex = 1;
             this.radList.TabStop = true;
             this.radList.Text = "Hiển thị theo Generic List";
             this.radList.UseVisualStyleBackColor = true;
-            this.radList.CheckedChanged += new System.EventHandler(this.radList_CheckedChanged);
+            this.radList.CheckedChanged += new System.EventHandler(this.radList_CheckedChanged_1);
             // 
             // radDataTable
             // 
             this.radDataTable.AutoSize = true;
-            this.radDataTable.Location = new System.Drawing.Point(26, 31);
-            this.radDataTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radDataTable.Location = new System.Drawing.Point(31, 44);
             this.radDataTable.Name = "radDataTable";
             this.radDataTable.Size = new System.Drawing.Size(138, 17);
             this.radDataTable.TabIndex = 2;
             this.radDataTable.TabStop = true;
             this.radDataTable.Text = "Hiển thị theo DataTable";
             this.radDataTable.UseVisualStyleBackColor = true;
-            this.radDataTable.CheckedChanged += new System.EventHandler(this.radDataTable_CheckedChanged);
+            this.radDataTable.CheckedChanged += new System.EventHandler(this.radDataTable_CheckedChanged_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 457);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.radDataTable);
             this.Controls.Add(this.radList);
             this.Controls.Add(this.dgvMember);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgvMember)).EndInit();
