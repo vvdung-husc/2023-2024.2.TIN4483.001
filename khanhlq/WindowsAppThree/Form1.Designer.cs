5407814c5f7fc48f238f5baa0724dc231a1d254a
lequangkhenh <lequangkhanhltqb@gmail.com>
2024-03-06 10:51:16 +0700

    khanhlq

--- khanhlq/WindowsAppThree/Form1.Designer.cs
+++ khanhlq/WindowsAppThree/Form1.Designer.cs
@@ -48,22 +48,24 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(37, 24);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.Size = new System.Drawing.Size(79, 20);
             this.radioButton1.TabIndex = 1;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Text = "radioList";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(35, 65);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.Size = new System.Drawing.Size(95, 20);
             this.radioButton2.TabIndex = 2;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.Text = "radioTable";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // Form1
             // 
--- khanhlq/WindowsAppThree/Program.cs
+++ khanhlq/WindowsAppThree/Program.cs
@@ -19,4 +19,20 @@ namespace WindowsAppThree
             Application.Run(new Form1());
         }
     }
+    public class Student
+    {
+        public string msv { get; set; }
+        public string ten { get; set; }
+        public string ngaysinh { get; set; }
+        public string khoahoc { get; set; }
+        public string nganhhoc { get; set; }
+        public Student(string id, string name, string birddate, string course, string subject)
+        {
+            this.msv = id;
+            this.ten = name;
+            this.ngaysinh = birddate;
+            this.khoahoc = course;
+            this.nganhhoc = subject;
+        }
+    }
 }
