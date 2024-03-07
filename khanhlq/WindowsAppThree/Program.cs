83b507bf1d08e83c6a1aceadd3d48b7dcc20c046
lequangkhenh <lequangkhanhltqb@gmail.com>
2024-03-07 14:28:33 +0700

    khanhlq

--- khanhlq/WindowsAppThree/Program.cs
+++ khanhlq/WindowsAppThree/Program.cs
@@ -19,20 +19,5 @@ namespace WindowsAppThree
             Application.Run(new Form1());
         }
     }
-    public class Student
-    {
-        public string msv { get; set; }
-        public string ten { get; set; }
-        public string ngaysinh { get; set; }
-        public string khoahoc { get; set; }
-        public string nganhhoc { get; set; }
-        public Student(string id, string ten, string ngay, string khoa, string nganh)
-        {
-            this.msv = id;
-            this.ten = ten;
-            this.ngaysinh = ngay;
-            this.khoahoc = khoa;
-            this.nganhhoc = nganh;
-        }
-    }
+   
 }
