497f6a3b5644032aa0aba0f08213372811986ecb
lequangkhenh <lequangkhanhltqb@gmail.com>
2024-03-11 13:46:55 +0700

    khanhlq

--- khanhlq/WindowsAppThree/Form1.cs
+++ khanhlq/WindowsAppThree/Form1.cs
@@ -10,22 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsAppThree
 {
-    public class Student
-    {
-        public string msv { get; set; }
-        public string ten { get; set; }
-        public string ngaysinh { get; set; }
-        public string khoahoc { get; set; }
-        public string nganhhoc { get; set; }
-        public Student(string id, string ten, string ngay, string khoa, string nganh)
-        {
-            this.msv = id;
-            this.ten = ten;
-            this.ngaysinh = ngay;
-            this.khoahoc = khoa;
-            this.nganhhoc = nganh;
-        }
-    }
+    
     public partial class Form1 : Form
     {
         protected List<Student> GetStudentByList()
--- khanhlq/WindowsAppThree/Program.cs
+++ khanhlq/WindowsAppThree/Program.cs
@@ -6,6 +6,22 @@ using System.Windows.Forms;
 
 namespace WindowsAppThree
 {
+    public class Student
+    {
+        public string msv { get; set; }
+        public string ten { get; set; }
+        public string ngaysinh { get; set; }
+        public string khoahoc { get; set; }
+        public string nganhhoc { get; set; }
+        public Student(string id, string ten, string ngay, string khoa, string nganh)
+        {
+            this.msv = id;
+            this.ten = ten;
+            this.ngaysinh = ngay;
+            this.khoahoc = khoa;
+            this.nganhhoc = nganh;
+        }
+    }
     internal static class Program
     {
         /// <summary>
