7a0aadd1bd839dd74913e0ade63de861aeea8d9d
HuyThanh02 <97724992+HuyThanh02@users.noreply.github.com>
2024-03-14 15:48:01 +0700

    Add files via upload

---
+++ TranHuyThanh/WindowsFormOrder/WindowsFormOrder/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿using System;
+
+namespace WindowsFormOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.btkhoaitc = new System.Windows.Forms.Button();
+            this.btcomga = new System.Windows.Forms.Button();
+            this.bpmca = new System.Windows.Forms.Button();
+            this.btcam = new System.Windows.Forms.Button();
+            this.btcoffe = new System.Windows.Forms.Button();
+            this.btgaranphan = new System.Windows.Forms.Button();
+            this.bpmtom = new System.Windows.Forms.Button();
+            this.btlipton = new System.Windows.Forms.Button();
+            this.bt7up = new System.Windows.Forms.Button();
+            this.btgvcola = new System.Windows.Forms.Button();
+            this.bpmga = new System.Windows.Forms.Button();
+            this.btcocola = new System.Windows.Forms.Button();
+            this.pepsi = new System.Windows.Forms.Button();
+            this.bttomvien = new System.Windows.Forms.Button();
+            this.bpmbo = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbblist = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(211, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fastfood Order";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.btkhoaitc);
+            this.groupBox.Controls.Add(this.btcomga);
+            this.groupBox.Controls.Add(this.bpmca);
+            this.groupBox.Controls.Add(this.btcam);
+            this.groupBox.Controls.Add(this.btcoffe);
+            this.groupBox.Controls.Add(this.btgaranphan);
+            this.groupBox.Controls.Add(this.bpmtom);
+            this.groupBox.Controls.Add(this.btlipton);
+            this.groupBox.Controls.Add(this.bt7up);
+            this.groupBox.Controls.Add(this.btgvcola);
+            this.groupBox.Controls.Add(this.bpmga);
+            this.groupBox.Controls.Add(this.btcocola);
+            this.groupBox.Controls.Add(this.pepsi);
+            this.groupBox.Controls.Add(this.bttomvien);
+            this.groupBox.Controls.Add(this.bpmbo);
+            this.groupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox.Location = new System.Drawing.Point(12, 59);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(581, 213);
+            this.groupBox.TabIndex = 1;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Danh Mục Món Ăn";
+            // 
+            // btkhoaitc
+            // 
+            this.btkhoaitc.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btkhoaitc.Location = new System.Drawing.Point(366, 161);
+            this.btkhoaitc.Name = "btkhoaitc";
+            this.btkhoaitc.Size = new System.Drawing.Size(185, 39);
+            this.btkhoaitc.TabIndex = 14;
+            this.btkhoaitc.Text = "Khoai tây chiên";
+            this.btkhoaitc.UseVisualStyleBackColor = true;
+            this.btkhoaitc.Click += new System.EventHandler(this.btkhoaitc_Click);
+            // 
+            // btcomga
+            // 
+            this.btcomga.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcomga.Location = new System.Drawing.Point(196, 161);
+            this.btcomga.Name = "btcomga";
+            this.btcomga.Size = new System.Drawing.Size(150, 39);
+            this.btcomga.TabIndex = 13;
+            this.btcomga.Text = "Cơm gà tender";
+            this.btcomga.UseVisualStyleBackColor = true;
+            this.btcomga.Click += new System.EventHandler(this.btcomga_Click);
+            // 
+            // bpmca
+            // 
+            this.bpmca.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bpmca.Location = new System.Drawing.Point(31, 161);
+            this.bpmca.Name = "bpmca";
+            this.bpmca.Size = new System.Drawing.Size(159, 39);
+            this.bpmca.TabIndex = 12;
+            this.bpmca.Text = "Burger phô mai cá";
+            this.bpmca.UseVisualStyleBackColor = true;
+            this.bpmca.Click += new System.EventHandler(this.bpmca_Click);
+            // 
+            // btcam
+            // 
+            this.btcam.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcam.Location = new System.Drawing.Point(459, 117);
+            this.btcam.Name = "btcam";
+            this.btcam.Size = new System.Drawing.Size(92, 38);
+            this.btcam.TabIndex = 11;
+            this.btcam.Text = "Cam";
+            this.btcam.UseVisualStyleBackColor = true;
+            this.btcam.Click += new System.EventHandler(this.btcam_Click);
+            // 
+            // btcoffe
+            // 
+            this.btcoffe.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcoffe.Location = new System.Drawing.Point(366, 117);
+            this.btcoffe.Name = "btcoffe";
+            this.btcoffe.Size = new System.Drawing.Size(75, 38);
+            this.btcoffe.TabIndex = 10;
+            this.btcoffe.Text = "Coffe";
+            this.btcoffe.UseVisualStyleBackColor = true;
+            this.btcoffe.Click += new System.EventHandler(this.btcoffe_Click);
+            // 
+            // btgaranphan
+            // 
+            this.btgaranphan.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btgaranphan.Location = new System.Drawing.Point(196, 117);
+            this.btgaranphan.Name = "btgaranphan";
+            this.btgaranphan.Size = new System.Drawing.Size(150, 38);
+            this.btgaranphan.TabIndex = 9;
+            this.btgaranphan.Text = "Gà rán phần";
+            this.btgaranphan.UseVisualStyleBackColor = true;
+            this.btgaranphan.Click += new System.EventHandler(this.btgaranphan_Click);
+            // 
+            // bpmtom
+            // 
+            this.bpmtom.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bpmtom.Location = new System.Drawing.Point(31, 117);
+            this.bpmtom.Name = "bpmtom";
+            this.bpmtom.Size = new System.Drawing.Size(159, 38);
+            this.bpmtom.TabIndex = 8;
+            this.bpmtom.Text = "Burger phô mai tôm";
+            this.bpmtom.UseVisualStyleBackColor = true;
+            this.bpmtom.Click += new System.EventHandler(this.bpmtom_Click);
+            // 
+            // btlipton
+            // 
+            this.btlipton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btlipton.Location = new System.Drawing.Point(459, 74);
+            this.btlipton.Name = "btlipton";
+            this.btlipton.Size = new System.Drawing.Size(92, 37);
+            this.btlipton.TabIndex = 7;
+            this.btlipton.Text = "Lipton";
+            this.btlipton.UseVisualStyleBackColor = true;
+            this.btlipton.Click += new System.EventHandler(this.btlipton_Click);
+            // 
+            // bt7up
+            // 
+            this.bt7up.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt7up.Location = new System.Drawing.Point(366, 74);
+            this.bt7up.Name = "bt7up";
+            this.bt7up.Size = new System.Drawing.Size(75, 37);
+            this.bt7up.TabIndex = 6;
+            this.bt7up.Text = "7 up";
+            this.bt7up.UseVisualStyleBackColor = true;
+            this.bt7up.Click += new System.EventHandler(this.bt7up_Click);
+            // 
+            // btgvcola
+            // 
+            this.btgvcola.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btgvcola.Location = new System.Drawing.Point(196, 74);
+            this.btgvcola.Name = "btgvcola";
+            this.btgvcola.Size = new System.Drawing.Size(150, 37);
+            this.btgvcola.TabIndex = 5;
+            this.btgvcola.Text = "Gà viên cola";
+            this.btgvcola.UseVisualStyleBackColor = true;
+            this.btgvcola.Click += new System.EventHandler(this.btgvcola_Click);
+            // 
+            // bpmga
+            // 
+            this.bpmga.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bpmga.Location = new System.Drawing.Point(31, 74);
+            this.bpmga.Name = "bpmga";
+            this.bpmga.Size = new System.Drawing.Size(159, 37);
+            this.bpmga.TabIndex = 4;
+            this.bpmga.Text = "Burger phô mai gà";
+            this.bpmga.UseVisualStyleBackColor = true;
+            this.bpmga.Click += new System.EventHandler(this.bpmga_Click);
+            // 
+            // btcocola
+            // 
+            this.btcocola.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcocola.Location = new System.Drawing.Point(459, 33);
+            this.btcocola.Name = "btcocola";
+            this.btcocola.Size = new System.Drawing.Size(92, 35);
+            this.btcocola.TabIndex = 3;
+            this.btcocola.Text = "Cocacola";
+            this.btcocola.UseVisualStyleBackColor = true;
+            this.btcocola.Click += new System.EventHandler(this.btcocola_Click);
+            // 
+            // pepsi
+            // 
+            this.pepsi.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pepsi.Location = new System.Drawing.Point(366, 34);
+            this.pepsi.Name = "pepsi";
+            this.pepsi.Size = new System.Drawing.Size(75, 34);
+            this.pepsi.TabIndex = 2;
+            this.pepsi.Text = "Pepsi";
+            this.pepsi.UseVisualStyleBackColor = true;
+            this.pepsi.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // bttomvien
+            // 
+            this.bttomvien.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttomvien.Location = new System.Drawing.Point(196, 34);
+            this.bttomvien.Name = "bttomvien";
+            this.bttomvien.Size = new System.Drawing.Size(150, 34);
+            this.bttomvien.TabIndex = 1;
+            this.bttomvien.Text = "Tôm viên coca";
+            this.bttomvien.UseVisualStyleBackColor = true;
+            this.bttomvien.Click += new System.EventHandler(this.bttomvien_Click);
+            // 
+            // bpmbo
+            // 
+            this.bpmbo.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bpmbo.Location = new System.Drawing.Point(31, 34);
+            this.bpmbo.Name = "bpmbo";
+            this.bpmbo.Size = new System.Drawing.Size(159, 34);
+            this.bpmbo.TabIndex = 0;
+            this.bpmbo.Text = "Burger Phô mai bò";
+            this.bpmbo.UseVisualStyleBackColor = true;
+            this.bpmbo.Click += new System.EventHandler(this.bpmbo_Click);
+            // 
+            // btXoa
+            // 
+            this.btXoa.Location = new System.Drawing.Point(43, 278);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(88, 39);
+            this.btXoa.TabIndex = 2;
+            this.btXoa.Text = "Xóa";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(471, 278);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(92, 39);
+            this.button17.TabIndex = 3;
+            this.button17.Text = "Order";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tên bàn";
+            // 
+            // cbblist
+            // 
+            this.cbblist.FormattingEnabled = true;
+            this.cbblist.Location = new System.Drawing.Point(246, 285);
+            this.cbblist.Name = "cbblist";
+            this.cbblist.Size = new System.Drawing.Size(207, 27);
+            this.cbblist.TabIndex = 5;
+            this.cbblist.Text = "tên bàn";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(520, 164);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(618, 499);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbblist);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "E-Order Application";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btkhoaitc_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button btlipton;
+        private System.Windows.Forms.Button bt7up;
+        private System.Windows.Forms.Button btgvcola;
+        private System.Windows.Forms.Button bpmga;
+        private System.Windows.Forms.Button btcocola;
+        private System.Windows.Forms.Button pepsi;
+        private System.Windows.Forms.Button bttomvien;
+        private System.Windows.Forms.Button bpmbo;
+        private System.Windows.Forms.Button btkhoaitc;
+        private System.Windows.Forms.Button btcomga;
+        private System.Windows.Forms.Button bpmca;
+        private System.Windows.Forms.Button btcam;
+        private System.Windows.Forms.Button btcoffe;
+        private System.Windows.Forms.Button btgaranphan;
+        private System.Windows.Forms.Button bpmtom;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbblist;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
