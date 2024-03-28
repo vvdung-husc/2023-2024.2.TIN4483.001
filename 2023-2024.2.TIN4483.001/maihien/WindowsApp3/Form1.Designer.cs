f6e38b43f2800301f7da016749985293d006a5bf
hien hien <hienhien27092001@gmail.com>
2024-03-28 16:17:07 +0700

    bb

---
+++ 2023-2024.2.TIN4483.001/maihien/WindowsApp3/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowAppThree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMember = new System.Windows.Forms.DataGridView();
+            this.radList = new System.Windows.Forms.RadioButton();
+            this.radDataTable = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMember)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMember
+            // 
+            this.dgvMember.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMember.Location = new System.Drawing.Point(26, 75);
+            this.dgvMember.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvMember.Name = "dgvMember";
+            this.dgvMember.RowHeadersWidth = 51;
+            this.dgvMember.RowTemplate.Height = 24;
+            this.dgvMember.Size = new System.Drawing.Size(781, 344);
+            this.dgvMember.TabIndex = 0;
+            this.dgvMember.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMember_CellContentClick);
+            // 
+            // radList
+            // 
+            this.radList.AutoSize = true;
+            this.radList.Location = new System.Drawing.Point(26, 10);
+            this.radList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radList.Name = "radList";
+            this.radList.Size = new System.Drawing.Size(144, 17);
+            this.radList.TabIndex = 1;
+            this.radList.TabStop = true;
+            this.radList.Text = "Hiển thị theo Generic List";
+            this.radList.UseVisualStyleBackColor = true;
+            this.radList.CheckedChanged += new System.EventHandler(this.radList_CheckedChanged);
+            // 
+            // radDataTable
+            // 
+            this.radDataTable.AutoSize = true;
+            this.radDataTable.Location = new System.Drawing.Point(26, 31);
+            this.radDataTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radDataTable.Name = "radDataTable";
+            this.radDataTable.Size = new System.Drawing.Size(138, 17);
+            this.radDataTable.TabIndex = 2;
+            this.radDataTable.TabStop = true;
+            this.radDataTable.Text = "Hiển thị theo DataTable";
+            this.radDataTable.UseVisualStyleBackColor = true;
+            this.radDataTable.CheckedChanged += new System.EventHandler(this.radDataTable_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 457);
+            this.Controls.Add(this.radDataTable);
+            this.Controls.Add(this.radList);
+            this.Controls.Add(this.dgvMember);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMember)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMember;
+        private System.Windows.Forms.RadioButton radList;
+        private System.Windows.Forms.RadioButton radDataTable;
+    }
+}
