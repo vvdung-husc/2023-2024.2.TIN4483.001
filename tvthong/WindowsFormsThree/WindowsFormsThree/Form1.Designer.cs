50dcc0638babc8e09f494be220cdbf22cf6a2856
thongvlog27 <20t1020574@husc.edu.vn>
2024-03-28 17:53:43 +0700

    ff

--- tvthong/WindowsFormsThree/WindowsFormsThree/Form1.Designer.cs
+++ tvthong/WindowsFormsThree/WindowsFormsThree/Form1.Designer.cs
@@ -88,7 +88,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 522);
+            this.ClientSize = new System.Drawing.Size(887, 522);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.radDataTable);
             this.Controls.Add(this.radList);
