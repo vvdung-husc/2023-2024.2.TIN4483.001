2fb6d2f0dff50a817fa760fdf23917fd9ff7d042
Nguyễn Phúc Tài <taiphuc192@gmail.com>
2024-03-30 17:37:16 +0700

    update

--- NguyenPhucTai/WindowsAppTwo/Properties/AssemblyInfo.cs
+++ NguyenPhucTai/WindowsAppTwo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleAppOne")]
+[assembly: AssemblyTitle("WindowsAppTwo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleAppOne")]
+[assembly: AssemblyProduct("WindowsAppTwo")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("30f4dd39-fe47-480d-8222-cf3a4c698de2")]
+[assembly: Guid("f7bc86cc-096e-40fc-bca9-c748273f02c7")]
 
 // Version information for an assembly consists of the following four values:
 //
