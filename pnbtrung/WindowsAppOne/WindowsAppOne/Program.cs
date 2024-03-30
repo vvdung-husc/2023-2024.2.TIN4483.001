1e7d072a381bb2e7a3b07c87f13b8e17fe498922
Trung2307 <inuyasa2020@gmail.com>
2024-03-30 08:50:29 +0700

    gg

--- pnbtrung/WindowsAppOne/WindowsAppOne/Program.cs
+++ pnbtrung/WindowsAppOne/WindowsAppOne/Program.cs
@@ -15,6 +15,8 @@ namespace WindowsAppOne
         [STAThread]
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
